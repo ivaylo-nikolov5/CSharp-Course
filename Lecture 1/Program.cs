3302c9e77209c688010b3fb379142fdaa43c0fe8
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-18 11:45:41 +0300

    Added a solution for the Projects Creation problem

---
+++ Lecture 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string town = Console.ReadLine();
+            
+            string result = $"You are {firstName} {lastName}, a {age}-years old person from {town}.";
+            Console.WriteLine(result);
+        }
+    }
+}
