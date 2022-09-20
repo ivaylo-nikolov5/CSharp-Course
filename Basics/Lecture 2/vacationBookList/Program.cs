99e026f5b6f4ff3734c438bf0b02a2be1f3cf349
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-20 15:17:23 +0300

    Added a solution for the Vacation Books List problem

---
+++ Basics/Lecture 2/vacationBookList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vacationBookList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPages = int.Parse(Console.ReadLine());
+            int pagesPerHour = int.Parse(Console.ReadLine());
+            int estimatedDays = int.Parse(Console.ReadLine());
+
+            int timeForTheBook = numberOfPages / pagesPerHour;
+            int hoursPerDay = timeForTheBook / estimatedDays;
+
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
