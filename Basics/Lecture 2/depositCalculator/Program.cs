bd464fb462b7db2c3aaf1b5fdf645f53f44f8742
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-20 13:40:24 +0300

    Added a asolution for the Deposit Calculator problem

---
+++ Basics/Lecture 2/depositCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace depositCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposedAmout = double.Parse(Console.ReadLine());
+            int depositTerm = int.Parse(Console.ReadLine());
+            double yearlyInterestPercent = double.Parse(Console.ReadLine());
+
+            double interest = deposedAmout * (yearlyInterestPercent / 100);
+            double monthInterest = interest / 12;
+            double total = deposedAmout + (depositTerm * monthInterest);
+
+            Console.WriteLine(total);
+        }
+    }
+}
