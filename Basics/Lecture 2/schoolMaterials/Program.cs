b3110fbb3adfc31664efa3310ae10173738fb322
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-20 15:25:17 +0300

    Added a solution for the Supplies for School problem

---
+++ Basics/Lecture 2/schoolMaterials/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schoolMaterials
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int penPackages = int.Parse(Console.ReadLine());
+            int markerPackages = int.Parse(Console.ReadLine());
+            int boardCleanerLiters = int.Parse(Console.ReadLine());
+            int percentDiscount = int.Parse(Console.ReadLine());
+
+            double pensPrice = penPackages * 5.8;
+            double markersPrice = markerPackages * 7.2;
+            double boardCleanerPrice = boardCleanerLiters * 1.2;
+
+            double totalPrice = pensPrice + markersPrice + boardCleanerPrice;
+            totalPrice -= totalPrice * (percentDiscount / 100.0);
+
+            Console.WriteLine(totalPrice);
+
+        }
+    }
+}
