cb028e84c25255e4e0a5a5193072c5e16a696476
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-20 15:43:12 +0300

    Added a solution for the Repainting problem

---
+++ Basics/Lecture 2/repainting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repainting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int metersNylon = int.Parse(Console.ReadLine());
+            int litersPaint = int.Parse(Console.ReadLine());
+            int litersThinner = int.Parse(Console.ReadLine());
+            int neededHours = int.Parse(Console.ReadLine());
+
+            double nylonPrice = (metersNylon + 2) * 1.5;
+            double paintPrice = (litersPaint + (litersPaint * 0.1)) * 14.5;
+            double thinnerPrice = litersThinner * 5;
+
+            double suppliesPrice = nylonPrice + paintPrice + thinnerPrice + 0.4;
+            double workersCost = (suppliesPrice * 0.3) * neededHours;
+
+            double totalPrice = suppliesPrice + workersCost;
+
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
