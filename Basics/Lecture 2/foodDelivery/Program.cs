12c5aeab80b18ffea66dfc5f6ab7e4f491a55fa1
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-20 16:45:27 +0300

    Added a solution for the Food Delivery problem

---
+++ Basics/Lecture 2/foodDelivery/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foodDelivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int chickenMenus = int.Parse(Console.ReadLine());
+            int fishMenus = int.Parse(Console.ReadLine());
+            int vegetarianMenus = int.Parse(Console.ReadLine());
+
+            double chickenMenusPrice = chickenMenus * 10.35;
+            double fishMenusPrice = fishMenus * 12.4;
+            double vegetarianMenusPrice = vegetarianMenus * 8.15;
+
+            double totalFoodPrice = chickenMenusPrice + fishMenusPrice + vegetarianMenusPrice;
+            double dessertPrice = 0.2 * totalFoodPrice;
+
+            double totalPrice = totalFoodPrice + dessertPrice +  2.5;
+
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
