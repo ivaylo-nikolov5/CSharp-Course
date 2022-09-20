42bab4644e7880c287b6144c237e53e7d7f955d6
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-20 16:52:20 +0300

    Added a solution for the Basketball Equipment problem

---
+++ Basics/Lecture 2/basketballEquipment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basketballEquipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yearlyTax = int.Parse(Console.ReadLine());
+
+            double sneakersPrice = yearlyTax - (yearlyTax * 0.4);
+            double basketballEquipPrice = sneakersPrice - (sneakersPrice * 0.2);
+            double ballPrice = basketballEquipPrice / 4;
+            double basketballAccessoriesPrice = ballPrice / 5;
+
+            double totalPrice = sneakersPrice + basketballEquipPrice + ballPrice + basketballAccessoriesPrice + yearlyTax;
+
+            Console.WriteLine(totalPrice);
+
+
+        }
+    }
+}
