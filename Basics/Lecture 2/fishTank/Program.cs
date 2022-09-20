49826bae6c6e23be589e3b00f21586b7f7353549
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-20 16:58:59 +0300

    Added asolution for the Fish Tank problem

---
+++ Basics/Lecture 2/fishTank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fishTank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine()) / 100;
+
+            int capacity = length * width * height;
+            double capacityInLiters = capacity * 0.001;
+            double neededLiters = capacityInLiters * (1 - percent);
+
+            Console.WriteLine(neededLiters);
+        }
+    }
+}
