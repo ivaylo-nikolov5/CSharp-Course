d79f3854f0a62a7cb1049f6603269149ee19d7f3
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-20 12:40:50 +0300

    Added a solution for the USD top BGN Problem

---
+++ Basics/Lecture 2/usdToBgnConvertor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usdToBgnConvertor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dollars = double.Parse(Console.ReadLine());
+            double levs = dollars * 1.79549;
+
+            Console.WriteLine(levs);
+        }
+    }
+}
