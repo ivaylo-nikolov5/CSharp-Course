6f2c8c95aadec24b7175c89d502aeb0920f82702
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-22 18:15:12 +0300

    Added a solution for the Password Guess problem

---
+++ Basics/Lecture 3/Lecture 3/gueessThePassword/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gueessThePassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchedPassword = "s3cr3t!P@ssw0rd";
+            string password = Console.ReadLine();
+
+            if (password == searchedPassword)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+
+        }
+    }
+}
