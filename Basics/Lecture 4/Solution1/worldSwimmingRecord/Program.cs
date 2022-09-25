75e570bbb0cf94c0db30cf80dd0d7e45874f5643
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-25 17:12:12 +0300

    Added a solution for the World Swimming Record problem

---
+++ Basics/Lecture 4/Solution1/worldSwimmingRecord/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace worldSwimmingRecord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordInSeconds = double.Parse(Console.ReadLine());
+            double distanceInMeters = double.Parse(Console.ReadLine());
+            double timePerMeter = double.Parse(Console.ReadLine());
+
+            double totalTime = distanceInMeters * timePerMeter;
+            totalTime += Math.Floor(distanceInMeters / 15) * 12.5;
+
+            double diff = Math.Abs(totalTime - recordInSeconds);
+
+            if (totalTime < recordInSeconds)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {totalTime:f2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {diff:f2} seconds slower.");
+            }
+        }
+    }
+}
