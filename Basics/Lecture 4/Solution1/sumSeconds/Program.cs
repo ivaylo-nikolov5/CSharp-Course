7b351ecfddd6da3e59fb7986fc0b882927a13652
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-25 14:10:25 +0300

    Added a solution for the Sum Seconds problem

---
+++ Basics/Lecture 4/Solution1/sumSeconds/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumSeconds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secondTime = int.Parse(Console.ReadLine());
+            int thirdTime = int.Parse(Console.ReadLine());
+
+            int totalSeconds = firstTime + secondTime + thirdTime;
+            int minutes = totalSeconds / 60;
+            int seconds = totalSeconds % 60;
+
+            if (seconds < 10)
+            {
+                Console.WriteLine($"{minutes}:0{seconds}");
+            }
+            else
+            {
+                Console.WriteLine($"{minutes}:{seconds}");
+            }
+
+
+        }
+    }
+}
