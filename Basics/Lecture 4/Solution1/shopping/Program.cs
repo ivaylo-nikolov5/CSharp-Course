b2ae755dcea637d42a3ffcb1d1e69e37ea7269c2
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-25 17:43:51 +0300

    dded a solution for the Shopping problem

---
+++ Basics/Lecture 4/Solution1/shopping/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int videoCards = int.Parse(Console.ReadLine());
+            int cpus = int.Parse(Console.ReadLine());
+            int rams = int.Parse(Console.ReadLine());
+
+            double videoCardsPrice = videoCards * 250;
+            double cpusPrice = cpus * (0.35 * videoCardsPrice);
+            double ramsPrice = rams * (0.1 * videoCardsPrice);
+
+            double totalPrice = videoCardsPrice + cpusPrice + ramsPrice;
+            if (videoCards > cpus)
+            {
+                totalPrice -= totalPrice * 0.15;
+            }
+
+            double diff = Math.Abs(totalPrice - budget);
+
+            if (budget >= totalPrice)
+            {
+                Console.WriteLine($"You have {diff:f2} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {diff:f2} leva more!");
+            }
+        }  
+    }
+}
