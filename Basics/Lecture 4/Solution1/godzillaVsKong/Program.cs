da712c5e8c3febe2ebcacf693cb8230a92ee7581
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-25 16:45:54 +0300

    Added a solution for the Godzilla vs. Kong problem

---
+++ Basics/Lecture 4/Solution1/godzillaVsKong/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace godzillaVsKong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double movieBudget = double.Parse(Console.ReadLine());
+            int statistsCount = int.Parse(Console.ReadLine());
+            double outfitPrice = double.Parse(Console.ReadLine());
+
+            double decorPrice = movieBudget * 0.1;
+            double clothingPrice = statistsCount * outfitPrice;
+
+            if (statistsCount > 150)
+            {
+                clothingPrice -= clothingPrice * 0.1;
+            }
+
+            double totalPrice = decorPrice + clothingPrice;
+            double diff = movieBudget - totalPrice;
+
+            if (diff >= 0)
+            {
+                Console.WriteLine($"Action!\nWingard starts filming with {diff:f2} leva left.\n");
+
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money!\nWingard needs {Math.Abs(diff):f2} leva more.");
+            }
+
+
+        }
+    }
+}
