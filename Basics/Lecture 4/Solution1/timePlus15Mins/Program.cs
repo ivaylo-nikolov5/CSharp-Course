c40182ceaef141403767b4c4d2515bedfc0fe0af
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-25 15:15:08 +0300

    Added a solution for the Time + 15 Minutes problem

---
+++ Basics/Lecture 4/Solution1/timePlus15Mins/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timePlus15Mins
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 15;
+
+            if (minutes > 59)
+            {
+                hours += 1;
+                minutes %= 60;
+            }
+
+            if (hours > 23)
+            {
+                hours = 0;
+
+            }
+
+            Console.WriteLine($"{hours}:{minutes:00}");
+        }
+
+    }
+}
