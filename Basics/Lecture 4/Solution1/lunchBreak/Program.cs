d1aede58912d41f3e89774a54033fd92c7c42a5c
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-25 18:10:26 +0300

    Added a solution for the Lunch Break problem

---
+++ Basics/Lecture 4/Solution1/lunchBreak/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lunchBreak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string serialName = Console.ReadLine();
+            int episodeLength = int.Parse(Console.ReadLine());
+            int breakLength = int.Parse(Console.ReadLine());
+
+            double lunchTime = breakLength / 8;
+            double restTime = breakLength / 4;
+            double timeLeft = Math.Ceiling(breakLength - lunchTime - restTime);
+
+            if (timeLeft >= episodeLength)
+            {
+                Console.WriteLine($"You have enough time to watch {serialName} and left with {Math.Ceiling(timeLeft - episodeLength)} minutes free time.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough time to watch {serialName}, you need {Math.Ceiling(episodeLength - timeLeft)} more minutes.");
+            }
+        }
+    }
+}
