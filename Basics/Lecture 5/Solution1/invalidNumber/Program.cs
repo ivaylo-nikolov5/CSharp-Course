39cf57e6b671ea5af95a8dd9a5132e192ddaa26b
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-28 18:24:30 +0300

    Added a solution for the Invalid Number problem

---
+++ Basics/Lecture 5/Solution1/invalidNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace invalidNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            if (!(number >= 100 && number <= 200 || number == 0))
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
