337230b08b3bc53dcd154c1eab158736c028ed82
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-28 11:26:51 +0300

    Added a solution for the Number in Range porblem

---
+++ Basics/Lecture 5/Solution1/numberInRange/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numberInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            if (number >= -100 && number <= 100 && number != 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+
+    }
+}
