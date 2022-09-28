b86f15a90a68d35bbea1434d95098efb2989a8e2
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-28 12:31:32 +0300

    Added a soluiton for the Cinema Ticket problem

---
+++ Basics/Lecture 5/Solution1/cinemaTicket/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cinemaTicket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+            int result = 0;
+
+            switch (day) 
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Friday":
+                    result = 12;
+                    break;
+                case "Wednesday":
+                case "Thursday":
+                    result = 14;
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    result = 16;
+                    break;
+
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
