c8d1dc54b5c9c3ded8928f3ebd4e6c49bc3a9e2b
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-28 11:36:26 +0300

    Added a solution for the Working Hours problem

---
+++ Basics/Lecture 5/Solution1/workingTime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workingTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            if (hour < 10 || hour > 18 || day == "Sunday")
+            {
+                Console.WriteLine("closed");
+            }
+            else if (hour >= 10 && hour <= 18)
+            {
+                Console.WriteLine("open");
+            }
+        }
+    }
+}
