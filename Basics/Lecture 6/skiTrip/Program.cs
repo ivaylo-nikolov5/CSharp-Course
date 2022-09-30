9f8dc4e2b437ca5f7f1999e9f783e1d8c9960177
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-30 15:28:28 +0300

    Added a solution for the Ski Trip problem

---
+++ Basics/Lecture 6/skiTrip/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+
+namespace skiTrip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine()) - 1;
+            string roomType = Console.ReadLine();
+            string rating = Console.ReadLine();
+
+            double price = 0;
+
+            if (roomType == "room for one person")
+            {
+                price = 18 * days;
+            }
+            else if (roomType == "apartment")
+            {
+                price = 25 * days;
+
+                if (days > 15)
+                {
+                    price -= price * 0.5;
+                }
+                else if (days >= 10)
+                {
+                    price -= price * 0.35;
+                }
+                else
+                {
+                    price -= price * 0.3;
+                }
+            }
+            else if (roomType == "president apartment")
+            {
+                price = 35 * days;
+                
+                if (days > 15)
+                {
+                    price -= price * 0.2;
+                }
+                else if (days >= 10)
+                {
+                    price -= price * 0.15;
+                }
+                else
+                {
+                    price -= price * 0.1;
+                }
+            }
+
+            if (rating == "positive")
+            {
+                price += price * 0.25;
+            }
+            else
+            {
+                price -= price * 0.1;
+            }
+
+            Console.WriteLine($"{price:f2}");
+
+            
+        }
+    }
+}
---
+++ Basics/Lecture 6/skiTrip/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("skiTrip")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("skiTrip")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9d6dc3ed-890b-4fe6-98b9-7a3273e22a8e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
