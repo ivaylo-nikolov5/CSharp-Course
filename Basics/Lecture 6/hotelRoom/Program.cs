40abc105e5bde202e7575e147fb906ad12e6689f
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-30 14:49:50 +0300

    Added a solution for the Hotel Room problem

---
+++ Basics/Lecture 6/hotelRoom/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotelRoom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int nights = int.Parse(Console.ReadLine());
+
+            double studioPrice = 0;
+            double apartmentPrice = 0;
+
+            if (month == "May" || month == "October")
+            {
+                studioPrice = 50 * nights;
+                apartmentPrice = 65 * nights;
+
+                if (nights > 7 && nights <= 14)
+                {
+                    studioPrice -= studioPrice * 0.05;
+                }
+                else if (nights > 14)
+                {
+                    studioPrice -= studioPrice * 0.3;
+                }
+            }
+
+            else if (month == "June" || month == "September")
+            {
+                studioPrice = 75.2 * nights;
+                apartmentPrice = 68.7 * nights;
+
+                if (nights > 14)
+                {
+                    studioPrice -= studioPrice * 0.2;
+                }
+            }
+            else
+            {
+                studioPrice = 76 * nights;
+                apartmentPrice = 77 * nights;
+            }
+
+            if (nights > 14)
+            {
+                apartmentPrice -= apartmentPrice * 0.1;
+            }
+
+            Console.WriteLine($"Apartment: {apartmentPrice:f2} lv.");
+            Console.WriteLine($"Studio: {studioPrice:f2} lv.");
+
+        }
+    }
+}
