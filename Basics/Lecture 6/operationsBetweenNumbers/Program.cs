ee21d1323472a85f37bdbc65328dac415b4a523c
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-30 14:33:00 +0300

    Added a solution for the Operations Between Numbers problem

---
+++ Basics/Lecture 6/operationsBetweenNumbers/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operationsBetweenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+
+            string result = "";
+
+            if (operation == "+")
+            {
+                int total = number1 + number2;
+                string evenOdd = "";
+                if (total % 2 == 0)
+                {
+                    evenOdd = "even";
+                }
+                else
+                {
+                    evenOdd = "odd";
+                }
+
+                result = $"{number1} + {number2} = {total} - {evenOdd}";
+            }
+
+            else if (operation == "-")
+            {
+                int total = number1 - number2;
+                string evenOdd = "";
+                if (total % 2 == 0)
+                {
+                    evenOdd = "even";
+                }
+                else
+                {
+                    evenOdd = "odd";
+                }
+
+                result = $"{number1} - {number2} = {total} - {evenOdd}";
+            }
+
+            else if (operation == "*")
+            {
+                int total = number1 * number2;
+                string evenOdd = "";
+                if (total % 2 == 0)
+                {
+                    evenOdd = "even";
+                }
+                else
+                {
+                    evenOdd = "odd";
+                }
+
+                result = $"{number1} * {number2} = {total} - {evenOdd}";
+            }
+
+            else if (operation == "/")
+            {
+
+                if (number2 == 0)
+                {
+                    result = $"Cannot divide {number1} by zero";
+                }
+                else
+                {
+
+                    double total = number1 * 1.0 / number2;
+
+                    result = $"{number1} / {number2} = {total:f2}";
+                }
+            }
+
+            else if (operation == "%")
+            {
+
+                if (number2 == 0)
+                {
+                    result = $"Cannot divide {number1} by zero";
+                }
+                else
+                {
+                    int total = number1 % number2;
+
+                    result = $"{number1} % {number2} = {total}";
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
