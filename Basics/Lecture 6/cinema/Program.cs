5a88cd8ff9defc0ce321dcc65ca66ca77df5d651
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-30 11:18:38 +0300

    Added a soluiton for the Cinema problem

---
+++ Basics/Lecture 6/cinema/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string projectionType = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+
+            double ticketPrice = 0;
+
+            if (projectionType == "Premiere")
+            {
+                ticketPrice = 12;
+            }
+            else if (projectionType == "Normal")
+            {
+                ticketPrice = 7.5;
+            }
+            else if (projectionType == "Discount")
+            {
+                ticketPrice = 5;
+            }
+
+            double result = ticketPrice * rows * cols;
+
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
