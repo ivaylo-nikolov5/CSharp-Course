5fa3226348752fff3c3f16f3140b386db1f9a086
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-10-02 14:59:43 +0300

    Added a solution for the Left and Right Sum problem

---
+++ Basics/Lecture 7/Solution1/leftAndRightSum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leftAndRightSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 0; i < numbersCount * 2; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (i < numbersCount)
+                {
+                    leftSum += number;
+                }
+                else
+                {
+                    rightSum += number;
+                }
+            }
+
+            if (rightSum == leftSum)
+            {
+                Console.WriteLine($"Yes, sum = {leftSum}");
+            }
+            else
+            {
+                int diff = Math.Abs(leftSum - rightSum);
+                Console.WriteLine($"No, diff = {diff}");
+            }
+        }
+    }
+}
---
+++ Basics/Lecture 7/Solution1/numberSequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numberSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+            int maxNum = int.MinValue;
+            int minNum = int.MaxValue;
+
+            for (int i = 0; i < numbersCount; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number > maxNum)
+                {
+                    maxNum = number;
+                }
+                else if (number < minNum)
+                {
+                    minNum = number;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNum}");
+            Console.WriteLine($"Min number: {minNum}");
+        }
+    }
+}
