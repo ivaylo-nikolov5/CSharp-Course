f3889c08c731039f2491c139265eccaf33628b87
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-10-02 13:08:48 +0300

    Added a solution for the  Character Sequence problem

---
+++ Basics/Lecture 7/Solution1/characterSequence/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace characterSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            for (int ch = 0; ch < str.Length; ch++)
+            {
+                Console.WriteLine(str[ch]);
+            }
+        }
+    }
+}
