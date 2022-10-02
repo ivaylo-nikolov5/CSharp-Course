8eb5c451f21b5d7f2bf37adda4602729846672ec
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-10-02 15:04:58 +0300

    Added a solution for the Odd Even Sum problem

---
+++ Basics/Lecture 7/Solution1/oddEvenSum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oddEvenSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = 1; i <= numbersCount; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    evenSum += number;
+                }
+                else
+                {
+                    oddSum += number;
+                }
+            }
+
+            if (oddSum == evenSum)
+            {
+                Console.WriteLine($"Yes");
+                Console.WriteLine($"Sum = {oddSum}");
+            }
+            else
+            {
+                int diff = Math.Abs(oddSum - evenSum);
+                Console.WriteLine($"No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
