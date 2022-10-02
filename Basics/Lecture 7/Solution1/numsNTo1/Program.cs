d77b73d8743518d9366a60e5a3b985b75a909451
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-10-02 11:02:04 +0300

    Added a solution for the Numbers N...1 problem

---
+++ Basics/Lecture 7/Solution1/numsNTo1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numsNTo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = number; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
