b89094014011912e1a84356850d80375e8fce007
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-10-02 11:18:47 +0300

    Added a solution for the Even Powers of 2 problem

---
+++ Basics/Lecture 7/Solution1/evenPowersOf2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenPowersOf2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= num; i += 2)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+        }
+    }
+}
